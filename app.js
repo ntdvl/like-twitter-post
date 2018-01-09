c62328708495fefd9d423661b2ebac81dbc00f08
ntdvl <yilmazkoman@outlook.com>
2018-01-09 21:05:14 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var first = document.getElementById('message');
+var second = document.getElementById('result');
+
+var charLimit = 180;
+
+first.onkeyup = function () {
+
+	second.innerHTML = charLimit - this.value.length;
+
+	if (second.innerHTML < 0) {
+
+		second.style.color = 'red';
+
+	}
+
+
+}
